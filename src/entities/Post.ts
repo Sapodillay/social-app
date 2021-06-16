a1b193ea958122e09650c8a8837e354ce6259c00
Sapodilla <jakebaxter8@hotmail.co.uk>
2021-06-16 15:45:18 +0100

    Added graphql type support

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,18 +1,25 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+
+@ObjectType()//Type decorator to convert into a graphql type
 @Entity()
 export class Post {
 
+	@Field(() => Int) //Field types for graphql
 	@PrimaryKey()
 	id!: number;
 
-	@Property()
+	@Field(() => String)
+	@Property({type: 'date'})
 	createdAt = new Date()
 
-	@Property({ onUpdate: () => new Date() })
+	@Field(() => String)
+	@Property({type: 'date', onUpdate: () => new Date() })
 	updatedAt = new Date();
 
-	@Property()
+	@Field(() => String)
+	@Property({type: 'text'})
 	title!: string;
 
 
