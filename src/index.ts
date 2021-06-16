9cced2b180a6205e79b670095d3f89dcdda41698
Sapodilla <jakebaxter8@hotmail.co.uk>
2021-06-16 14:55:19 +0100

    Setup mikro-orm config and migrations

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import { MikroORM } from '@mikro-orm/core'
-
+import { Post } from './entities/Post';
+import mikroConfig from './mikro-orm.config'
 
 const main = async () => {
-	const orm = await MikroORM.init({
-		dbName: 'social-app',
-		user: 'postgres',
-		password: 'postgres',
-		debug: true
-	}); 
+	const orm = await MikroORM.init(mikroConfig); 
 
+
+	const post = orm.em.create(Post, {title: "first post"})
+	await orm.em.persistAndFlush(post)
 }
 
 main()
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,16 @@
+import { Post } from "./entities/Post";
+import { MikroORM } from '@mikro-orm/core'
+import path from 'path'
+
+export default {
+	migrations: {
+		path: path.join(__dirname + './migrations'), // path to folder with migration files
+		pattern: /^[\w-]+\d+\.[tj]s$/, // how to match migration files
+	},
+	entities: [Post],
+	dbName: 'social-app',
+	type: 'postgresql',
+	user: 'postgres',
+	password: 'postgres',
+	debug: true
+} as Parameters<typeof MikroORM.init>[0];
