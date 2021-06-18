2f0a310423962017170fc2fc15ab629352eeacaf
Sapodilla <jakebaxter8@hotmail.co.uk>
2021-06-18 14:33:18 +0100

    Added user resolver and register mutation

---
+++ src/resolvers/user.ts
@@ -0,0 +1,30 @@
+import { User } from "src/entities/User";
+import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import argon2 from 'argon2';
+import { MyContext } from "src/types";
+
+
+@InputType()
+class UsernamePasswordInput {
+	@Field()
+	username: string;
+	@Field()
+	password: string;
+}
+
+
+
+@Resolver()
+export class UserResolver{
+	@Mutation(() => String)
+	async register(
+		@Arg('options') options: UsernamePasswordInput,
+		@Ctx() { em }: MyContext
+	){
+		const hashedPassword = await argon2.hash(options.password)
+		const user = em.create(User, {username: options.password, password: hashedPassword})
+		await em.persistAndFlush(user)
+		
+
+	}
+}
